8066006e1f7b603c14bbccbb175f7ec0216c0b33
TinePretnar <73723936+TinePretnar@users.noreply.github.com>
2023-07-06 17:09:30 +0200

    yep

---
+++ DiplomskaSpringBoot/src/main/java/com/example/DiplomskaSpringBoot/DiplomskaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiplomskaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskaSpringBootApplication.class, args);
+	}
+
+}
---
+++ DiplomskaSpringBoot/src/test/java/com/example/DiplomskaSpringBoot/DiplomskaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiplomskaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
